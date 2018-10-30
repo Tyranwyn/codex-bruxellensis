dade0c42cf5270f7fee915e05c321939a6c7904f
Sammi Fux <sammi.fux@capac-hvw.fgov.be>
2018-10-30 15:48:54 +0100

    updated bricklayers lament, la biere and ben laden

--- insert-song.js
+++ insert-song.js
@@ -1,6 +1,6 @@
 // npm install firebase-admin
 const admin = require('firebase-admin');
-var serviceAccount = require('./codex-bruxellensis-5616053ee25e.json'); // download key from google cloud
+var serviceAccount = require('./codex-bruxellensis-firebase-adminsdk-z44yg-c52182ac92.json'); // download key from google cloud
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -9,17 +9,22 @@ admin.initializeApp({
 var db = admin.firestore();
 
 const song = {
-                   "title": "Ruiterslied",
-                   "bgInfo": "T: Georg Herwegh, Burschenschaft Patrioten Tübingen, 1841\nM: Justus Wilhelm Lyra, Burschenschaft Knorschia Bonn, 1843",
-                   "lyrics": "Dit lied wordt door de laatstejaars gezongen. Voor ze beginnen worden de glazen gevuld.\n\nDe bange nacht is weeral om,\nWe rijden stil, we rijden stom,\nWe rijden ten verderve!\nHoe koud waait toch de morgenwind!\nWeerdin, nu nog een glas gezwind\nVoor ’t sterven.\nHoe staat het jonge gras nu groen,\nMaar bloeden zal het morgen doen,\nMijn eigen bloed zal ’t verven\nDe eerste slok, met ’t zweerd in hand,\nGedronken voor het vaderland\nVoor ’t sterven.\nHier wordt een eerste slok gedronken\nDe tweede slok van d’edele wijn,\nZal voor de heilige vrijheid zijn,\nVoor vrijheid, have en erve;\nHier wordt de tweede slok gedronken\nJ In Memoriam Bertholet, La Curée, tijdschrift van CPL, novem-\nber 1956. Ter gelegenheid van de zwanezang van één van de comitards\nvan CPL.\n\nDe rest zij nog een huldeblijk,\nDe laatst aan ’t oud Romeinse rijk\nVoor ’t sterven.\nHier wordt het glas geledigd\nVoor ’t liefken, maar mijn glas is uit,\nDe spere blinkt, de kogel fluit,\nDraag aan mijn kind de scherven,\nHier wordt het glas kapotgegooid\nVooruit nu naar de laatste slag.\nO, ruiterslust in vroege dag.\nEn sterven.",
+                   "title": "La Bière",
+                   "bgInfo": "T & M: Antoine Clesse (Bergen)\nWerd oorspronkelijk op een andere melodie gezongen.",
+                   "lyrics": "Elle a vraiment d’une bière flamande\nL’air avenant, l’éclat et la douceur,\nJoyeux Wallons, elle nous a?riande\nEt le Faro trouve en elle une soeur.\nÀ plein verre, mes bons amis,\nEn la buvant, il faut chanter la bière,\nÀ plein verre, mes bons amis.\nIl faut chanter la bière du pays.\n\nVoyez là-bas la kermesse en délire:\nLes pots sont pleins, jouez ménétriers!\nQuels jeux bruyants et quels éclats de rire!\nCe sont encore les Flamands de Teniers!\nAux souverains, portant tout haut leurs plaintes,\nBourgeois jaloux des droits de la cité,\nNos francs aîeux, tout en vidant leur pinte,\nFondaient les arts avec la liberté.\nQuand leurs tribuns à l’attitude altière,\nFaisaient sonner le tocsin des be?rois,\nTous ces fumeurs, tous ces buveurs de Bière,\nSavaient combattre et mourir pour leurs droits.\nBelges, chantons, à ce refrein à boire,\nPeintres, guerriers qui nous illustrent tous,\nGéants couchés dans leur linceul de gloire,\nVont se lever, pour redire avec nous.\nSalut à toi, Bière limpide et blonde!\nJe tiens mon verre, et le bonheur en main,\nAh! J’en voudrais verser à tout le monde,\nPour le bonheur de tout le genre humain.",
                    "associationName": "",
                    "associationInfo": "",
                    "battleCryName": "",
                    "battleCryInfo": "",
                    "battleCry": "",
-                   "page": 215,
-                   "category": "DUTCH"
+                   "page": 278,
+                   "category": "FRENCH"
                  };
+
+// Update exisiting
+/*var updateDoc = db.collection('songs')
+    .doc('rTZ3d34I8xGwA0A5wH2M')
+    .set(song);*/
 
 // Add a new document with a generated id.
 var addDoc = db.collection('songs').add(song).then(ref => {
