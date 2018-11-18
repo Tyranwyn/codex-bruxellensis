23bb1a72b89dfd57ea2b7c5859936c5c103214cd
tyranwyn <sammi.fux@hotmail.com>
2018-11-18 18:19:43 +0100

    updated some more songs

--- insert-song.js
+++ insert-song.js
@@ -1,6 +1,6 @@
 // npm install firebase-admin
 const admin = require('firebase-admin');
-var serviceAccount = require('./codex-bruxellensis-firebase-adminsdk-z44yg-c52182ac92.json'); // download key from google cloud
+var serviceAccount = require('./codex-bruxellensis-firebase-adminsdk-z44yg-f1c5a31819.json'); // download key from google cloud
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -9,21 +9,21 @@ admin.initializeApp({
 var db = admin.firestore();
 
 const song = {
-                   "title": "La Bière",
-                   "bgInfo": "T & M: Antoine Clesse (Bergen)\nWerd oorspronkelijk op een andere melodie gezongen.",
-                   "lyrics": "Elle a vraiment d’une bière flamande\nL’air avenant, l’éclat et la douceur,\nJoyeux Wallons, elle nous a?riande\nEt le Faro trouve en elle une soeur.\nÀ plein verre, mes bons amis,\nEn la buvant, il faut chanter la bière,\nÀ plein verre, mes bons amis.\nIl faut chanter la bière du pays.\n\nVoyez là-bas la kermesse en délire:\nLes pots sont pleins, jouez ménétriers!\nQuels jeux bruyants et quels éclats de rire!\nCe sont encore les Flamands de Teniers!\nAux souverains, portant tout haut leurs plaintes,\nBourgeois jaloux des droits de la cité,\nNos francs aîeux, tout en vidant leur pinte,\nFondaient les arts avec la liberté.\nQuand leurs tribuns à l’attitude altière,\nFaisaient sonner le tocsin des be?rois,\nTous ces fumeurs, tous ces buveurs de Bière,\nSavaient combattre et mourir pour leurs droits.\nBelges, chantons, à ce refrein à boire,\nPeintres, guerriers qui nous illustrent tous,\nGéants couchés dans leur linceul de gloire,\nVont se lever, pour redire avec nous.\nSalut à toi, Bière limpide et blonde!\nJe tiens mon verre, et le bonheur en main,\nAh! J’en voudrais verser à tout le monde,\nPour le bonheur de tout le genre humain.",
+                   "title": "La Fille De La Bouchère",
+                   "bgInfo": "Gebaseerd op waargebeurde feiten;\nFestival de la chanson estudiantine 1998",
+                   "lyrics": "Je me souviens de la fille de la bouchère,\nElle était vraiment conne comme sa mère,\nMais quand on avait bu assez de bière,\nOn lui mettait dans son derrière.\nSortant du Wapps en état plus potable,\nEnvie de niquer cette pétasse sur la table,\nTellement bourré ma vision très minable,\nJ’ai éclaté un pot de salade de crabe.\nEn douceur, en douceur, en douceur et profondeur.\nL’était chouette la fille de la bouchère,\n(Schwing, schwing, schwing)\nElle nous faisait bander comme des cerfs.\n\nAprès St V et beaucoup de chopines,\nJe voulais bien lui faire sucer ma pine,\nLa tête pressée trop près de sa poitrine,\nEt sur mon bout collait de la gélatine.\nLe soir d’un fût il m’fallait de la baise,\nEntr’les jambons je me sentais bien à l’aise,\nJe lui ai mis ma saucisse ardennaise,\nBien lubrifiée avec d’la dijonnaise.\nEn douceur, en douceur, j’ai tripoté sa petite fleur.\nRefrein\nElle s’était réfugiée dans le frigo,\nLa porte s’est fermée derrière mon dos,\nJe me suis fait lécher mon Calippo,\nPar une gigantesque tête de veau.\nJ’ai raconté toutes mes chaudes histoires,\nDe sorte que tout le Cercle y aille la voir,\nEt complètement bourré comme tous les soirs,\nOn sautait tout ce qu’y avait dans le comptoir.\nMoraal:\nLa morale de cette histoire est ferme,\nBoire et baiser ça tue le commerce.\nLa boucherie s’est fermée car à long terme,\nToute la viande y goûtait fort le sperme.\nEh encore, eh encore,\nAu Cercle Popo on est des porcs.\nRefrein ad libitum",
                    "associationName": "",
                    "associationInfo": "",
                    "battleCryName": "",
                    "battleCryInfo": "",
                    "battleCry": "",
-                   "page": 278,
+                   "page": 331,
                    "category": "FRENCH"
                  };
 
 // Update exisiting
 /*var updateDoc = db.collection('songs')
-    .doc('rTZ3d34I8xGwA0A5wH2M')
+    .doc('pwYsLjFesZkgw6yWrnCE')
     .set(song);*/
 
 // Add a new document with a generated id.
